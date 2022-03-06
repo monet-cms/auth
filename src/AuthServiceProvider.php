15a161826e219b162762b11d3d107f855d722c20
Kieron Sutton <sutton.kieron99@gmail.com>
2022-03-06 00:08:31 +0000

    Fix provider

--- src/AuthServiceProvider.php
+++ src/AuthServiceProvider.php
@@ -6,12 +6,15 @@ use Illuminate\Support\ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    public function boot()
+    public function register(): void
     {
         $this->mergeConfigFrom(
             __DIR__ . '/../config/auth.php', 'monet.auth'
         );
+    }
 
+    public function boot(): void
+    {
         $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
 
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'monet.auth');
@@ -20,14 +23,14 @@ class AuthServiceProvider extends ServiceProvider
             return;
         }
 
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+
         $this->publishes([
-            __DIR__ . '/../config/auth.php' => config_path('monet-auth.php')
-        ]);
+            __DIR__ . '/../config/auth.php' => config_path('monet.auth.php')
+        ], 'config');
 
         $this->publishes([
             __DIR__ . '/../resources/views' => resource_path('views/vendor/monet/auth')
-        ]);
-
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        ], 'views');
     }
 }
