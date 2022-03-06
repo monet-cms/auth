573817c699f98f530a0f9a1827afe42a437b08a7
Kieron Sutton <sutton.kieron99@gmail.com>
2022-03-06 03:41:25 +0000

    Use the latest support

--- src/AuthServiceProvider.php
+++ src/AuthServiceProvider.php
@@ -2,35 +2,21 @@
 
 namespace Monet\Framework\Auth;
 
-use Illuminate\Support\ServiceProvider;
+use Monet\Framework\Support\Services\Package;
+use Monet\Framework\Support\Services\ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    public function register(): void
+    public function configurePackage(Package $package): void
     {
-        $this->mergeConfigFrom(
-            __DIR__ . '/../config/auth.php', 'monet.auth'
-        );
-    }
-
-    public function boot(): void
-    {
-        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
-
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'monet.auth');
-
-        if (!$this->app->runningInConsole()) {
-            return;
-        }
-
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
-
-        $this->publishes([
-            __DIR__ . '/../config/auth.php' => config_path('monet.auth.php')
-        ], 'config');
-
-        $this->publishes([
-            __DIR__ . '/../resources/views' => resource_path('views/vendor/monet/auth')
-        ], 'views');
+        $package
+            ->name('monet.auth')
+            ->hasConfig()
+            ->hasRoute('web.php')
+            ->hasViews()
+            ->hasMigrations([
+                'create_users_table',
+                'create_password_resets_table'
+            ]);
     }
 }
--- src/Models/User.php
+++ src/Models/User.php
@@ -6,7 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Monet\Framework\Support\Extendable;
+use Monet\Framework\Support\Extensions\Extendable;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
